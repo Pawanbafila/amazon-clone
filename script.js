14f07265eb7879b482e8f0d0440a2a92790e473f
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-03-22 02:11:39 +0530

    add side bar

---
+++ script.js
@@ -0,0 +1,10 @@
+function showbar(){
+    const bar = document.querySelector('.sidenav')
+    bar.style.display = 'block'
+}
+
+function hidebar(){
+    const hide = document.querySelector('.sidenav')
+    hide.style.display = 'none'
+
+}
