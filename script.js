874be018b51295c05b1f10da50e01ad486fea27b
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-03-23 17:53:04 +0530

    add sign up form

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function hidebar(){
     hide.style.display = 'none'
 
 }
+
+
+
+function showsign(){
+    const bar = document.querySelector('.hidden-sign')
+    bar.style.display = 'block'
+}
+
+function hidesign(){
+    const hide = document.querySelector('.hidden-sign')
+    hide.style.display = 'none'
+
+}
